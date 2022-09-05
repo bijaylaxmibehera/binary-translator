99a5309ceacb67840558a292941aadffdebfce72
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2022-09-05 13:06:07 +0530

    done

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ var outputDiv = document.querySelector("#output")
 
 // outputDiv.innerText = "Bijaylaxmi Behera"
 // console.log(outputDiv)
+var serverURL = "https://api.funtranslations.com/translate/numbers.json";
+
+function getTranslationURL(text) {
+   return serverURL + "?" + "text=" + text;
+}
 
 function eventHandler() {
-   console.log("clicked!");
-   console.log("input", textInput.value);
-   outputDiv.innerText = "abnac " + textInput.value;
+   //taking input
+   var inputText = textInput.value
+   //call server for processing
+   fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+         var translatedText = json.contents.translated;
+         outputDiv.innerText = translatedText; // output
+        })
 }
 btnTranslate.addEventListener("click", eventHandler)
